7ecc63fe5a635be542b9bd58855b933839e451b5
Sean Lucius <seanlucius22@icloud.com>
2019-01-17 05:26:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+    elsif array.length == 2 
+    array.join(" and ")
+  else
+    last_element = ", and #{array.pop}"
+    array.join(", ") + last_element
+  end 
 end
